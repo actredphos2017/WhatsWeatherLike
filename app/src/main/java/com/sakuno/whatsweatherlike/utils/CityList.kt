513f72f820df89958926506705e4ca052086f3cc
Sakunoakarinn <43726721+actredphos2017@users.noreply.github.com>
2023-01-25 17:50:23 +0800

    Switch to new city list

--- app/src/main/java/com/sakuno/whatsweatherlike/utils/CityList.kt
+++ app/src/main/java/com/sakuno/whatsweatherlike/utils/CityList.kt
@@ -4,64 +4,40 @@ import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
 
 
-data class CityList(
-    @SerializedName("municipalities") val municipalities: List<CityLocation>,
-    @SerializedName("other") val other: List<CityLocation>,
-    @SerializedName("provinces") val provinces: List<ProvinceLocation>
-) {
+class CityList : ArrayList<NewCityListItem>() {
+
     companion object {
         fun fromJsonString(jsonString: String): CityList = try {
             Gson().fromJson(jsonString, CityList::class.java)
         } catch (_: Exception) {
-            CityList(listOf(), listOf(), listOf())
-        }
-    }
-
-    private fun toCity(ori: Pair<CityLocation, String>): City {
-        return ori.first.coordinate.split(',', '|').run {
-            City(
-                longitude = (getOrNull(0))?.toDouble() ?: 110.0,
-                latitude = (getOrNull(1))?.toDouble() ?: 20.0,
-                showName = (if(ori.second.isBlank()) "" else "${ori.second} ") + ori.first.name,
-                isLocal = false
-            )
+            arrayListOf<NewCityListItem>() as CityList
         }
     }
 
     fun searchCity(key: String): Array<City> {
         val resList = mutableListOf<City>()
 
-        for (it in municipalities) if (it.name.indexOf(key) >= 0) resList += toCity(Pair(it, ""))
-
-        for (it in other) if (it.name.indexOf(key) >= 0) resList += toCity(Pair(it, ""))
-
-        for (it in provinces) {
-            if (it.name.indexOf(key) >= 0) resList += toCity(
-                Pair(
-                    CityLocation(
-                        it.coordinate,
-                        it.name
-                    ), ""
-                )
-            )
-            for (each in it.cities) if (each.name.indexOf(key) >= 0) resList += toCity(
-                Pair(
-                    each,
-                    it.name
-                )
-            )
-        }
+        for (it in this) if (it.province.indexOf(key) >= 0 || it.city.indexOf(key) >= 0 || it.district.indexOf(key) >= 0
+        ) resList += it.toCity()
 
         return resList.toTypedArray()
     }
+
 }
 
-data class CityLocation(
-    @SerializedName("g") val coordinate: String, @SerializedName("n") val name: String
-)
+data class NewCityListItem(
+    @SerializedName("city") val city: String,
+    @SerializedName("cityId") val cityId: String,
+    @SerializedName("district") val district: String,
+    @SerializedName("latitude") val latitude: String,
+    @SerializedName("longitude") val longitude: String,
+    @SerializedName("province") val province: String
+) {
+    fun toCity(): City = City(
+        longitude = longitude.toDouble(),
+        latitude = latitude.toDouble(),
+        showName = "${if (province != city) "$province " else ""}${city}${if (district == city) "" else " $district"}",
+        isLocal = false
+    )
 
-data class ProvinceLocation(
-    @SerializedName("cities") val cities: List<CityLocation>,
-    @SerializedName("g") val coordinate: String,
-    @SerializedName("n") val name: String
-)
+}
